07d5955f7bc69c08474e8ac711f57ec7f8ef1e41
BerkayEkebas <ekebasahmet@gmail.com>
2025-05-12 14:19:05 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import { AuthProvider } from "./Context/authContext.js"; // AuthProvider'ı import et
 import axios from 'axios';
@@ -12,7 +12,7 @@ axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter >
     <AuthProvider>
       <NavProvider>
       <Layout>
@@ -20,7 +20,7 @@ root.render(
       </Layout>
       </NavProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter >
 );
 
 // If you want to start measuring performance in your app, pass a function
